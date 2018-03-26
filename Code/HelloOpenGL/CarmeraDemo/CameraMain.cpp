6e237b4a21de95e28116916194cec48edc6a10ef
SixGodZhang <739536111@qq.com>
2018-03-26 14:14:00 +0800

    AddProject:LightDemo
    增加项目LightDemo

--- Code/HelloOpenGL/CarmeraDemo/CameraMain.cpp
+++ Code/HelloOpenGL/CarmeraDemo/CameraMain.cpp
@@ -60,6 +60,8 @@ int main()
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetScrollCallback(window, scroll_callback);
 
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
