0907c9166c7558a684c4271858cfc251224ca09e
SixGodZhang <739536111@qq.com>
2018-03-27 19:25:08 +0800

    Add Project: LiightMapDemo
    Add Project: LiightMapDemo

--- SettingCode/Shader/matLightFsShader.fs
+++ SettingCode/Shader/matLightFsShader.fs
@@ -35,7 +35,7 @@ void main()
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(lightPos - FragPos);
 	float diff = max(dot(norm,lightDir),0);
-	vec3 diffuse = light.diffuse * diff * (diff * material.diffuse);
+	vec3 diffuse = light.diffuse (diff * material.diffuse);
 	
 	//specular
 	float specularStrength;
