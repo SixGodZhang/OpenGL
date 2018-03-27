a37bd77a1b48e42b5e34da48f6338f5b0c573370
SixGodZhang <739536111@qq.com>
2018-03-27 17:10:19 +0800

    Add Project: MaterialDemo
    Add Project: MaterialDemo

---
+++ SettingCode/Shader/matLightFsShader.fs
@@ -0,0 +1,49 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 Normal;
+in vec3 FragPos;
+
+struct Material{
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	float skininess;
+};
+
+struct Light{
+	vec3 position;
+	
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+uniform Material material;
+uniform Light light;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+void main()
+{
+	//ambient
+	float ambientStrength = 0.1;
+	vec3 ambient = light.ambient * ambientStrength * material.ambient;
+	
+	//diffuse
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(lightPos - FragPos);
+	float diff = max(dot(norm,lightDir),0);
+	vec3 diffuse = light.diffuse * diff * (diff * material.diffuse);
+	
+	//specular
+	float specularStrength;
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir,norm);
+	float spec = pow(max(dot(viewDir,reflectDir),0.0),material.skininess);
+	vec3 specular = light.specular * specularStrength * (spec * material.specular);
+	
+	vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+}
