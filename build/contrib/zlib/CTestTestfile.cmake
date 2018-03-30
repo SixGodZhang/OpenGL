376a8064988b6c3fca63e5086260cec440c10b67
SixGodZhang <739536111@qq.com>
2018-03-30 18:10:45 +0800

    add Model relative files
    add Model relative files

---
+++ build/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/zhanghui/Desktop/OpenGLCode/assimp-master/contrib/zlib
+# Build directory: D:/OpenGL/build/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
