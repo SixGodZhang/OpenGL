afbdb34adaf6a018de57bf0a70da64cdea41ffb9
SixGodZhang <739536111@qq.com>
2018-04-02 09:54:00 +0800

    delete some files
    delete some files

--- build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/zhanghui/Desktop/OpenGLCode/assimp-master/contrib/zlib
-# Build directory: D:/OpenGL/build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
